ee24fa41a175aa397583a39495badccb9b080cb5
Poonam <poonam.chauhan229@gmail.com>
2022-09-19 12:55:52 +0530

    day 1 and day 2 intro|| datatypes

---
+++ 2day-Introduction/node_stripped.js
@@ -0,0 +1,15 @@
+
+// 1
+setTimeout(()=>{
+    console.log("Hi all")
+},2000)
+//2
+console.log("Helo Everyone")
+//3
+console.log("Welcome to GUvi")
+//4
+console.log("How are you all")
+
+var a=20
+        var b=30
+        console.log(a+b)
